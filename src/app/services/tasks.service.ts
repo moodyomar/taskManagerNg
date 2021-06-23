737ba5da0641fa57338911b0d92eded861055276
moodyomar <lonak.secret@gmail.com>
2021-06-23 08:45:49 +0300

    add funcs too complete single tasks + styling

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,45 +1,54 @@
 import { Injectable } from '@angular/core';
-import {sortBy} from "lodash";
+import { sortBy } from "lodash";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class TasksService {
-  tasks_ar:any[] = [
-    {name:"go to work", time:"09:00",id:1},
-    {name:"go home", time:"16:00",id:2},
+  tasks_ar: any[] = [
+    { name: "go to work", time: "09:00", id: 1 },
+    { name: "go home", time: "16:00", id: 2 },
   ]
+  completedTasks: any[] = []
   constructor() { }
 
-  getList():any{
-    if(localStorage["tasks"]){
+  getList(): any {
+    if (localStorage["tasks"]) {
       this.tasks_ar = JSON.parse(localStorage["tasks"]);
     }
     return this.tasks_ar;
   }
 
-  addNewTask(_task:any):void{
+  addNewTask(_task: any): void {
     this.tasks_ar.push(_task);
-    let temp_ar = sortBy(this.tasks_ar,"time")
-    this.tasks_ar.splice(0,this.tasks_ar.length);
+    let temp_ar = sortBy(this.tasks_ar, "time")
+    this.tasks_ar.splice(0, this.tasks_ar.length);
     this.tasks_ar.push(...temp_ar);
     this.saveToLocal();
   }
 
-  resetTasks():void{
-    this.tasks_ar.splice(0,this.tasks_ar.length);
+  resetTasks(): void {
+    this.tasks_ar.splice(0, this.tasks_ar.length);
     this.saveToLocal();
   }
 
-  removeSingleTask(_idDel:any):void{
-    let indexToDel = this.tasks_ar.findIndex(item =>  item.id == _idDel  )
-    this.tasks_ar.splice(indexToDel,1);
+  removeSingleTask(_idDel: any): void {
+    let indexToDel = this.tasks_ar.findIndex(item => item.id == _idDel)
+    this.tasks_ar.splice(indexToDel, 1);
     this.saveToLocal();
   }
+  completeTask(_id: any): void {
+    let taskToComplete = this.tasks_ar.filter(item => item.id == _id)
+    let indexToComplete = this.tasks_ar.findIndex(item => item.id == _id)
+    this.completedTasks.push(...taskToComplete);
+      this.tasks_ar.splice(indexToComplete, 1);
+    this.saveToLocal();
+    console.log(this.completedTasks)
+  }
 
-  saveToLocal():void{
-    localStorage.setItem('tasks',JSON.stringify(this.tasks_ar));
+  saveToLocal(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks_ar));
   }
 
 
--- src/app/task-manager/list-task/list-task.component.ts
+++ src/app/task-manager/list-task/list-task.component.ts
@@ -8,6 +8,7 @@ import { TasksService } from '../../services/tasks.service';
 })
 export class ListTaskComponent implements OnInit {
   tasks_ar:any[] = [];
+  isCompleted:boolean = false;
   constructor(private tasksSer:TasksService) { }
 
   ngOnInit(): void {
@@ -17,5 +18,11 @@ export class ListTaskComponent implements OnInit {
   delSingleTask(_idDel:any):void{
     this.tasksSer.removeSingleTask(_idDel)
   }
-
+  completeSingleTask(_id:any):void{
+setTimeout(() => {
+  this.tasksSer.completeTask(_id)
+}, 1000);
+// this.isCompleted = !this.isCompleted;
+console.log('animate')
+  }
 }
